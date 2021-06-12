632a82910cc28c27c2d534f2e6d04a19d0c2201d
dilbud <40974307+dilbud@users.noreply.github.com>
2021-06-12 22:34:08 +0530

    update

--- gitcheckoutAll.sh
+++ gitcheckoutAll.sh
@@ -1,4 +1,4 @@
-
 #!/bin/bash
+# EOF if windows to linux or if linux to windows
 
 remote=origin ; for brname in `git branch -r | grep $remote | grep -v /master | grep -v /HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'`; do git branch --track $brname $remote/$brname || true; done 2>/dev/null
